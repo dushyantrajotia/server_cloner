9136e0bad6b481b33318b52fa3506a61c784fbd9
dushyantrajotia <dushyantrajotia@gmail.com>
2025-05-10 16:45:44 +0530

    Initial Commit

---
+++ install_deps.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Install Python packages
+# pip install psutil
+# pip install pypresence
+# pip install discord
+# pip install asyncio
+# pip install colorama
+# pip install fore
+# pip install pystyle
+# pip3 install --user psutil
+# pip3 install --user pypresence
+# pip3 install --user discord
+# pip3 install --user asyncio
+# pip3 install --user colorama
+# pip3 install --user platforms
+# pip3 install discord==1.7.3
+# pip3 install discord.py==1.7.3
+pip install --break-system-packages psutil
+pip install --break-system-packages pypresence
+pip install --break-system-packages discord==1.7.3
+pip install --break-system-packages discord.py==1.7.3
+pip install --break-system-packages asyncio
+pip install --break-system-packages colorama
+pip install --break-system-packages fore
+pip install --break-system-packages pystyle
